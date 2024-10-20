d9e4adc82a43858d7f30bef043d755befa882222
vinay-clutch <vinayvinay0256@gmail.com>
2024-10-20 18:34:52 +0530

    Add files via upload

---
+++ password/generator/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+import random
+def home(request):
+  return render(request,'generator/home.html')
+
+def password(request):
+  characters=list('abcdefghijklmnopqrstuvwxyz')
+  if request.GET.get('uppercase'):
+    characters.extend(list('ABCDEFGHIJKLMNOPQRSTUVWXYZ'))
+  if request.GET.get('numbers'):
+    characters.extend(list('0123456789'))
+  if request.GET.get('special'):
+    characters.extend(list('@!%$&*+-'))
+  length=int(request.GET.get('length' , 8))
+  thepassword=" "
+  for i in range(length):
+     thepassword+=random.choice(characters)
+
+  return render(request,'generator/password.html',{'password':thepassword})
